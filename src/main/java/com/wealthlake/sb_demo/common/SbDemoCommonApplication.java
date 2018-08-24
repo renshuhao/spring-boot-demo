c93ca417ce3d01d6e1f2f81144de51d744589d3a
rensh <rensh@wealthlake.com.cn>
2018-08-24 18:18:18 +0800

    Initial commit

---
+++ src/main/java/com/wealthlake/sb_demo/common/SbDemoCommonApplication.java
@@ -0,0 +1,12 @@
+package com.wealthlake.sb_demo.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemoCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDemoCommonApplication.class, args);
+	}
+}
